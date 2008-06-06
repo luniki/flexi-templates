ca8e3e1cea020cb8ec3303218ed9fe83c2d7a486
Marcus Lunzenauer <mlunzena@uos.de>
2008-06-06 10:19:59 +0200

    new #cycle method to cycle through several values
    esp useful for odd/even classes in HTML table rows

--- lib/helper/text_helper.php
+++ lib/helper/text_helper.php
@@ -35,6 +35,14 @@
 class TextHelper {
 
   /**
+   * Holds the internal cycles.
+   *
+   * @ignore
+   */
+  private static $cycles = array();
+
+
+  /**
    * Returns a camelized string from a lower case and underscored string by
    * replacing slash with underscore and upper-casing each letter preceded
    * by an underscore.
@@ -47,6 +55,130 @@ class TextHelper {
     return str_replace(' ', '',
                        ucwords(str_replace(array('_', '/'),
                                            array(' ', ' '), $word)));
+  }
+
+
+  /**
+   * Creates a Cycle object whose +__toString method cycles through elements of
+   * an array every time it is called. This can be used for example, to
+   * alternate classes for table rows:
+   *
+   *   <? foreach ($items as $item) : ?>
+   *     <tr class="<?= TextHelper::cycle('odd', 'even') ?>">
+   *       <td><?= item ?></td>
+   *     </tr>
+   *   <? endforeach ?>
+   *
+   * You can use named cycles to allow nesting in loops.  Passing a single array
+   * as the only parameter with a <tt>name</tt> key will create a named cycle.
+   * You can manually reset a cycle by calling reset_cycle and passing the
+   * name of the cycle.
+   *
+   *   <? foreach($items as $item) : ?>
+   *     <tr class="<?= TextHelper::cycle(array("even", "odd", "name" => "row_class")) ?>">
+   *       <td>
+   *         <? foreach ($item->values as $value) : ?>
+   *           <span style="color:<?= TextHelper::cycle(array("red", "green", "blue", "name" => "colors")) : ?>">
+   *             <?= $value ?>
+   *           </span>
+   *         <? endforeach ?>
+   *         <? TextHelper::reset_cycle("colors") ?>
+   *       </td>
+   *    </tr>
+   *  <? endforeach ?>
+   */
+  static function cycle($first_value) {
+
+    $values = func_get_args();
+
+
+    if (sizeof($values) == 1 && is_array($values[0])) {
+      $values = $values[0];
+      $name = isset($values['name']) ? $values['name'] : 'default';
+      unset($values['name']);
+    }
+    else {
+      $name = 'default';
+    }
+
+    $cycle = self::get_cycle($name);
+    if (is_null($cycle) || $cycle->values !== $values) {
+      $cycle = self::set_cycle($name, new TextHelperCycle($values));
+    }
+
+    return (string) $cycle;
+  }
+
+
+  /**
+   * Resets a cycle so that it starts from the first element the next time
+   * it is called. Pass in +name+ to reset a named cycle.
+   *
+   * @param  string   an optional name of a cycle
+   *
+   * @return void
+   */
+  function reset_cycle($name = 'default') {
+    $cycle = self::get_cycle($name);
+    if (isset($cycle)) {
+      $cycle->reset();
+    }
+  }
+
+
+  /**
+   * @ignore
+   */
+  private static function get_cycle($name) {
+    return isset(self::$cycles[$name]) ? self::$cycles[$name] : NULL;
+  }
+
+  /**
+   * @ignore
+   */
+  private static function set_cycle($name, $cycle) {
+    return self::$cycles[$name] = $cycle;
+  }
+}
+
+
+/**
+ * This class holds an array of string and cycles through them.
+ *
+ * @package     flexi
+ * @subpackage  helper
+ *
+ * @author    Marcus Lunzenauer (mlunzena@uos.de)
+ * @copyright (c) Authors
+ * @version   $Id$
+ */
+
+class TextHelperCycle {
+
+
+  public $values;
+
+
+  function __construct($values) {
+    $this->values = (array) $values;
+  }
+
+
+  function cycle() {
+    $result = current($this->values);
+    if (next($this->values) === FALSE)
+      $this->reset();
+    return $result;
+  }
+
+
+  function reset() {
+    reset($this->values);
+  }
+
+
+  function __toString() {
+    return $this->cycle();
   }
 }
 
